c2861d0d22dc35338fc7b7bcb953955f5a442927
Arima057nn <tjendung97pain@gmail.com>
2024-02-06 13:03:17 +0700

    fetch api complete request

--- src/app/(user)/admin/processing/page.tsx
+++ src/app/(user)/admin/processing/page.tsx
@@ -8,50 +8,40 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { ProcessingManager } from "@/app/models/manager";
-import ItemManagerProcess from "@/app/components/itemManagerProcess";
-
-function createData(
-  id: string,
-  name: string,
-  content: number,
-  duedate: string,
-  category: number,
-  status: number
-) {
-  return { id, name, content, duedate, category, status };
-}
-
-const rows = [
-  createData("1", "Frozen yoghurt", 159, "6.0", 24, 4.0),
-  createData("2", "Ice cream sandwich", 237, "9.0", 37, 4.3),
-  createData("3", "Eclair", 262, "16.0", 24, 6.0),
-  createData("4", "Cupcake", 305, "3.7", 67, 4.3),
-  createData("5", "Gingerbread", 356, "16.0", 49, 3.9),
-];
+import { RequestInterface } from "@/app/models/request";
+import ItemAdminProcessing from "@/app/components/itemAdminProcessing";
+import { requestApi } from "@/app/services/request-api";
 
 export default function Processing() {
+  const [requests, setRequests] = React.useState<RequestInterface[]>([]);
+
+  React.useEffect(() => {
+    getRequests();
+  }, []);
+
+  const getRequests = async () => {
+    let res = await requestApi.getRequestByAdminId(5);
+    setRequests(res.data || []);
+  };
   return (
     <div className="p-4 flex flex-col items-center">
-      <p className="text-xl font-bold my-4">Processing</p>
+      <p className="text-xl font-bold my-4">List Request</p>
 
       <TableContainer component={Paper}>
         <Table>
           <TableHead sx={{ bgcolor: "#3887BE" }}>
             <TableRow>
               <TableCell sx={{ color: "#fff" }}>Id</TableCell>
-              <TableCell sx={{ color: "#fff" }}>Name</TableCell>
-              <TableCell sx={{ color: "#fff" }}>Content</TableCell>
+              <TableCell sx={{ color: "#fff" }}>Title</TableCell>
               <TableCell sx={{ color: "#fff" }}>Due Date</TableCell>
               <TableCell sx={{ color: "#fff" }}>Category</TableCell>
               <TableCell sx={{ color: "#fff" }}>Status</TableCell>
-              <TableCell sx={{ color: "#fff" }}>Comment</TableCell>
               <TableCell sx={{ color: "#fff" }}>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row: ProcessingManager) => (
-              <ItemManagerProcess item={row} key={row.id} />
+            {requests.map((request: RequestInterface) => (
+              <ItemAdminProcessing item={request} key={request._id} />
             ))}
           </TableBody>
         </Table>
---
+++ src/app/components/itemAdminProcessing/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { IconButton, TableCell, TableRow } from "@mui/material";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import { RequestInterface } from "@/app/models/request";
+import { CheckCircleOutline } from "@mui/icons-material";
+import { requestApi } from "@/app/services/request-api";
+import { AxiosError } from "axios";
+import { useState } from "react";
+
+const ItemAdminProcessing: React.FC<{
+  item: RequestInterface;
+}> = ({ item }) => {
+  const [approve, setApprove] = useState(true);
+
+  const handleChange = async (status: number) => {
+    try {
+      const res = await requestApi.changeStatusRequest(item._id, status);
+      console.log(res);
+      setApprove(false);
+    } catch (e) {
+      const error = e as AxiosError;
+      // toast.error(error.message);
+    }
+  };
+  return (
+    <TableRow key={item._id}>
+      <TableCell>{item._id}</TableCell>
+      <TableCell>{item.title}</TableCell>
+      <TableCell>{item.createdAt}</TableCell>
+      <TableCell>{item.categoryId.name}</TableCell>
+      <TableCell>{item.status}</TableCell>
+
+      <TableCell sx={{ display: "flex" }}>
+        <IconButton
+          aria-label="approve"
+          size="large"
+          onClick={() => handleChange(6)}
+        >
+          <CheckCircleOutline />
+        </IconButton>
+        <IconButton aria-label="delete" size="large">
+          <RemoveRedEyeIcon />
+        </IconButton>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default ItemAdminProcessing;
